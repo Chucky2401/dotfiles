1aae974510c8d409df55b1d462cc0a793f790ad1
The Black Wizard <chuckydu24@gmail.com>
2024-11-04 14:02:14 +0100

    feat: add syntax for Caddyfile

--- .config/nvim/lua/tbw/plugins/caddyfile.lua
+++ .config/nvim/lua/tbw/plugins/caddyfile.lua
@@ -1,3 +1,11 @@
-return {
-  "isobit/vim-caddyfile",
+local plugins = {
+  {
+    "isobit/vim-caddyfile",
+    ft = "caddyfile"
+  }
 }
+return plugins
+
+-- return {
+--   "isobit/vim-caddyfile",
+-- }
