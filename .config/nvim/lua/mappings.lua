5a6af6a53b9c974cfea88acddee3cc161958f0e1
The Black Wizard <chuckydu24@gmail.com>
2025-03-21 00:01:16 +0100

    feat(nvim): add markdown-preview

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -5,15 +5,16 @@ require "nvchad.mappings"
 local map = vim.keymap.set
 -- local default_opts = {noremap = true}
 
--- map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
--- map('n', '<leader>ff', "<cmd>lua require'telescope.builtin'.find_files({ find_command = {'rg', '--files', '--hidden', '-g', '!.git' }})<cr>", default_opts)
--- map('n', '<leader>fr', "<cmd>lua require'telescope.builtin'.buffers({ show_all_buffers = true })<cr>", default_opts)
--- map('n', '<leader>fg', "<cmd>lua require'telescope.builtin'.git_status()<cr>", default_opts)
--- map('n', '<leader>f?', ":TodoTelescope<cr>", default_opts)
+
+-- Telescope files
 map('n', '<leader>ff', "<cmd>lua require'telescope.builtin'.find_files({ find_command = {'rg', '--files', '--hidden', '-g', '!.git' }})<cr>", { desc = "Find all files" })
 map('n', '<leader>fr', "<cmd>lua require'telescope.builtin'.buffers({ show_all_buffers = true })<cr>", { desc = "Show all buffers" })
 map('n', '<leader>fg', "<cmd>lua require'telescope.builtin'.git_status()<cr>", { desc = "Show Git status" })
 map('n', '<leader>f?', ":TodoTelescope<cr>", { desc = "Show telescope todo" })
 
+-- Markdown Preview
+map('n', '<leader>mp', "<cmd>MarkdownPreview<cr>", { desc = "Start Markdown preview" })
+map('n', '<leader>ms', "<cmd>MarkdownPreviewStop<cr>", { desc = "Stop Markdown preview" })
+map('n', '<leader>mt', "<cmd>MarkdownPreivewToggle<cr>", { desc = "Toggle Markdown preview" })
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
