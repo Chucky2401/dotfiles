1200737f0ac5ab120ebe3ed4562aaf6bbf29f637
The Black Wizard <chuckydu24@gmail.com>
2025-04-02 21:57:07 +0200

    feat(nvim): add markdown-preview

---
+++ .config/nvim/lua/plugins/markdown-preview.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    "iamcco/markdown-preview.nvim",
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    build = "cd app && yarn install",
+    init = function()
+      vim.g.mkdp_filetypes = { "markdown" }
+    end,
+    ft = { "markdown" },
+    config = function()
+      vim.g.mkdp_open_to_the_world = 1
+      vim.g.mkdp_open_ip = "127.0.0.1"
+      vim.g.mkdp_port = 18080
+    end,
+  },
+}
