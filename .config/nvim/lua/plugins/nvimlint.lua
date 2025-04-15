72d26b299ba6e5446b877ecf120d5a7fa69352af
The Black Wizard <chuckydu24@gmail.com>
2025-04-15 22:13:26 +0200

    feat(nvim): Set config file for markdown linter

---
+++ .config/nvim/lua/plugins/nvimlint.lua
@@ -0,0 +1,14 @@
+return {
+  "mfussenegger/nvim-lint",
+  opts = {
+    linters = {
+      ["markdownlint-cli2"] = {
+        args = {
+          "--config",
+          vim.fn.stdpath("config") .. "/lua/plugins/cfg_linters/.markdownlint-cli2.jsonc",
+          "--",
+        },
+      },
+    },
+  },
+}
