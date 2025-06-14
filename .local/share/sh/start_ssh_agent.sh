b1e3356c30a2ff3219fe0146c8991ebd716d61fe
The Black Wizard <chuckydu24@gmail.com>
2025-06-14 16:48:19 +0200

    chore(ssh_agent): remove message printing

--- .local/share/sh/start_ssh_agent.sh
+++ .local/share/sh/start_ssh_agent.sh
@@ -3,20 +3,18 @@ function start_ssh_agent() {
   HOME_SSH_PID="${HOME}/.ssh/ssh_agent.pid"
 
   if [ -z "$SSH_AGENT_PID" ]; then
-    echo "SSH_AGENT_PID is empty"
     SSH_AGENT_PID=$(cat "$HOME_SSH_PID")
   fi
 
   if ! kill -0 $SSH_AGENT_PID &>/dev/null; then
-    echo "SSH_AGENT_PID (${SSH_AGENT_PID}) pid does not exist"
     rm "$HOME_SSH_SOCK" &>/dev/null
     message "Starting SSH agent..."
     eval $(ssh-agent -a "$HOME_SSH_SOCK") &>/dev/null
     echo "$SSH_AGENT_PID" >"$HOME_SSH_PID"
     ssh-add -k
     success_message "SSH agent start with '$HOME_SSH_SOCK'"
-  else
-    message "SSH agent on '$HOME_SSH_SOCK' ($(cat $HOME_SSH_PID))"
+  # else
+  #   message "SSH agent on '$HOME_SSH_SOCK' ($(cat $HOME_SSH_PID))"
   fi
 
   export SSH_AUTH_SOCK="$HOME_SSH_SOCK"
