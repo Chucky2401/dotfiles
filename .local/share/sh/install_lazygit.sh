4c3b3a455d652f760a1d2e3ab3edbe9dd79234ef
The Black Wizard <chuckydu24@gmail.com>
2025-06-21 17:38:53 +0200

    chore(install_lazygit): remove sudo as it is useless

--- .local/share/sh/install_lazygit.sh
+++ .local/share/sh/install_lazygit.sh
@@ -6,31 +6,29 @@ install_lazygit() {
     return
   fi
 
-	ARCH=$(uname -m)
-	if [ "$ARCH" = "aarch64" ]; then
-		ARCH="arm64"
-	fi
-	
-	LG_SETUP_FOLDER="/opt/lazygit/"
-	# LAZYGIT_VERSION_GIT=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*' > /dev/null 2>&1)
-	LAZYGIT_VERSION_GIT=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
+  ARCH=$(uname -m)
+  if [ "$ARCH" = "aarch64" ]; then
+    ARCH="arm64"
+  fi
 
-	LAZYGIT_VERSION_INSTALLED="0.0.0"
-	
-	if [[ ! -d "$LG_SETUP_FOLDER" ]]; then
-		mkdir $LG_SETUP_FOLDER
-	fi
-	
-	if [[ -d "$LG_SETUP_FOLDER" && -f /opt/lazygit/last_version ]]; then
-		LAZYGIT_VERSION_INSTALLED=$(cat /opt/lazygit/last_version)
-	fi
-	
-	if version_lessthan "$LAZYGIT_VERSION_INSTALLED" "$LAZYGIT_VERSION_GIT"; then
-		curl -sLo /opt/lazygit/lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION_GIT}_Linux_${ARCH}.tar.gz"
-		tar xf /opt/lazygit/lazygit.tar.gz -C /opt/lazygit/ lazygit
-		sudo install -Dm 755 /opt/lazygit/lazygit -t /usr/local/bin
-		rm -f /opt/lazygit/lazygit*
-		echo "$LAZYGIT_VERSION_GIT" > /opt/lazygit/last_version
-	fi
+  LG_SETUP_FOLDER="/opt/lazygit/"
+  LAZYGIT_VERSION_GIT=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
+
+  LAZYGIT_VERSION_INSTALLED="0.0.0"
+
+  if [[ ! -d "$LG_SETUP_FOLDER" ]]; then
+    mkdir $LG_SETUP_FOLDER
+  fi
+
+  if [[ -d "$LG_SETUP_FOLDER" && -f /opt/lazygit/last_version ]]; then
+    LAZYGIT_VERSION_INSTALLED=$(cat /opt/lazygit/last_version)
+  fi
+
+  if version_lessthan "$LAZYGIT_VERSION_INSTALLED" "$LAZYGIT_VERSION_GIT"; then
+    curl -sLo /opt/lazygit/lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION_GIT}_Linux_${ARCH}.tar.gz"
+    tar xf /opt/lazygit/lazygit.tar.gz -C /opt/lazygit/ lazygit
+    install -Dm 755 /opt/lazygit/lazygit -t /usr/local/bin
+    rm -f /opt/lazygit/lazygit*
+    echo "$LAZYGIT_VERSION_GIT" >/opt/lazygit/last_version
+  fi
 }
-
