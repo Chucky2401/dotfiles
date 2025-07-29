8883230497399606aa56214a6faf3371ae202916
The Black Wizard <chuckydu24@gmail.com>
2025-07-29 18:30:47 +0200

    feat(ssh): add hostname in fzf preview

--- .local/share/sh/ssh.sh
+++ .local/share/sh/ssh.sh
@@ -1,6 +1,10 @@
 function conn_ssh() {
   while read -r client; do
-    INFO=$(ssh -Gt "$client" 2>/dev/null | grep -E "^user |port " | tr '\n' ';' | sed 's/;$//')
+    INFO=$(ssh -Gt "$client" 2>/dev/null |
+      grep -E "^user |^port |^hostname " |
+      sort |
+      tr '\n' ';' |
+      sed 's/;$//')
     printf "%s:%s\n" "$client" "$INFO"
   done < <(grep -P "^Host ([^*]+)$" ~/.ssh/config | sed 's/Host //' | sort) | fzf --tmux center \
     --prompt ' ' \
@@ -8,7 +12,7 @@ function conn_ssh() {
     --border-label ' SSH Servers ' \
     --delimiter=':' \
     --with-nth '{1}' \
-    --preview 'echo {2} | sed "s/;/\n/"' \
+    --preview 'echo {2} | sed "s/;/\n/g"' \
     --bind 'enter:become(ssh {1})'
 }
 
