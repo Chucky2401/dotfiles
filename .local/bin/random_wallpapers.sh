ed21cf601d1c19fafb6331d7deff82df7e51341f
The Black Wizard <chuckydu24@gmail.com>
2025-08-16 14:15:19 +0200

    feat(hyprpaper): script to have a random wallpaper

---
+++ .local/bin/random_wallpapers.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+ROOT_WALLPAPER_DIR="$HOME/.wallpapers/"
+TYPE_WALLPAPER="${ROOT_WALLPAPER_DIR}$1/"
+CURRENT_WALLPAPER=$(hyprctl hyprpaper listloaded)
+
+if [[ -z "$1" && "$CURRENT_WALLPAPER" == "no wallpapers loaded" ]]; then
+  TYPE_WALLPAPER="$(find "$ROOT_WALLPAPER_DIR" -maxdepth 1 -type d | tail -n +2 | shuf -n 1)/"
+fi
+
+if [[ -z "$1" && "$CURRENT_WALLPAPER" != "no wallpapers loaded" ]]; then
+  TYPE_WALLPAPER="${ROOT_WALLPAPER_DIR}$(echo "$CURRENT_WALLPAPER" | awk 'BEGIN { FS = "/" } ; { print $5 }')/"
+fi
+
+WALLPAPER=$(find "$TYPE_WALLPAPER" -type f ! -name "$(basename "$CURRENT_WALLPAPER")" | shuf -n 1)
+
+hyprctl hyprpaper reload ,"contain:$WALLPAPER"
