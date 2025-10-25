8464991bf54369fe36175bc71e4d5b17ceddf904
The Black Wizard <chuckydu24@gmail.com>
2025-10-25 16:08:15 +0200

    feat(zsh): add alias for weather with wttr.in

--- zsh/.config/zsh/config.d/20-aliases.zsh
+++ zsh/.config/zsh/config.d/20-aliases.zsh
@@ -27,6 +27,7 @@ alias ld="lazydocker"
 alias fman="compgen -c | fzf | xargs man"
 alias dfh="echo "" ; df -h | tail -n +2 | column -t -N Device,Size,Used,Available,\"Used %\",Mountpoint ; echo"
 alias cleardns="resolvectl flush-caches"
+alias weather="curl wttr.in/Saint-Jean-Pla-De-Corts"
 
 # Alias Docker Pi3
 alias syncpi3='rsync -avze "ssh -p 666" /home/docker_pi3/* --progress blackwizard@192.168.1.40:/home/docker/'
