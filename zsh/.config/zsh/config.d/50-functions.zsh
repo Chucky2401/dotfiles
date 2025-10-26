74ba6aa3a4b12e01236f38a01c1feca1106d92bc
The Black Wizard <chuckydu24@gmail.com>
2025-10-26 13:12:14 +0100

    feat(zsh): add function to edit current cmd in vim

--- zsh/.config/zsh/config.d/50-functions.zsh
+++ zsh/.config/zsh/config.d/50-functions.zsh
@@ -15,3 +15,41 @@ function zle-line-init {
 
 zle -N zle-keymap-select
 zle -N zle-line-init
+
+#
+# Edit current line in editor
+#
+: ${ZVM_TMPDIR:=${TMPDIR:-/tmp}}
+: ${ZVM_VI_EDITOR:=${EDITOR:-vim}}
+
+function zvm_vi_edit_command_line() {
+  # Create a temporary file and save the BUFFER to it
+  local tmp_file=$(mktemp ${ZVM_TMPDIR}/zshXXXXXX)
+
+  # Some users may config the noclobber option to prevent from
+  # overwriting existing files with the > operator, we should
+  # use >! operator to ignore the noclobber.
+  echo "$BUFFER" >! "$tmp_file"
+
+  # Edit the file with the specific editor, in case of
+  # the warning about input not from a terminal (e.g.
+  # vim), we should tell the editor input is from the
+  # terminal and not from standard input.
+  "${(@Q)${(z)${ZVM_VI_EDITOR}}}" $tmp_file </dev/tty
+
+  # Reload the content to the BUFFER from the temporary
+  # file after editing, and delete the temporary file.
+  BUFFER=$(cat "$tmp_file")
+  rm "$tmp_file"
+
+  # Exit the visual mode
+  case $KEYMAP in
+    "visual")
+      zle visual-mode
+      ;;
+  esac
+}
+
+zle -N zvm_vi_edit_command_line
+bindkey -M visual 'v' zvm_vi_edit_command_line
+
