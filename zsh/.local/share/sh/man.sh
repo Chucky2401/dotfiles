a2420ca6ead1104ddf5f916e249cdcd0a7e4a1ad
Chucky2401 <chuckydu24@gmail.com>
2026-03-09 10:26:05 +0100

    fix(zsh): use var for bat binary name
    On Debian it is 'batcat' but all others distribution is 'bat'...

--- zsh/.local/share/sh/man.sh
+++ zsh/.local/share/sh/man.sh
@@ -1,9 +1,13 @@
 function fm() {
+  BAT_BIN="bat"
+  if ! [[ "$OS_NAME" =~ "Debian|Ubuntu" ]]; then
+    BAT_BIN="batcat"
+  fi
   /usr/bin/man -k . | awk '{printf "%s:%s", $1, gensub(/\(|\)/,"","g",$2); printf "\n"}' | sort | fzf \
     --delimiter=':' \
     --with-nth '{1}.{2}' \
-    --preview 'man {2} {1} | bat -l man -p' \
-    --bind 'enter:become(man {2} {1} | bat -l man -p)'
+    --preview "man {2} {1} | ${BAT_BIN} -l man -p" \
+    --bind "enter:become(man {2} {1} | ${BAT_BIN} -l man -p)"
 }
 
 function mn() {
